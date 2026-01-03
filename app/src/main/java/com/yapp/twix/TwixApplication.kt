35f51659cded7836d402fd4e8ab373abc4735226
dogmania <hj296914@gmail.com>
2026-01-03 22:29:32 +0900

    🔥 Remove: 불필요한 임포트 제거

--- app/src/main/java/com/yapp/twix/TwixApplication.kt
+++ app/src/main/java/com/yapp/twix/TwixApplication.kt
@@ -2,8 +2,6 @@ package com.yapp.twix
 
 import android.app.Application
 import com.yapp.twix.di.initKoin
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
 
 class TwixApplication : Application() {
     override fun onCreate() {
