781d97bb26ccb855315d4f2a772e0e46c4516b57
dogmania <hj296914@gmail.com>
2026-01-14 02:18:35 +0900

    ✨ Feat: Color 시스템 구현

---
+++ core/design-system/src/main/java/com/twix/designsystem/theme/Colors.kt
@@ -0,0 +1,26 @@
+package com.twix.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+object GrayColor {
+    val C050 = Color(0xFFF4F4F4)
+    val C100 = Color(0xFFECECEC)
+    val C200 = Color(0xFFC6C6C6)
+    val C300 = Color(0xFF858585)
+    val C400 = Color(0xFF464646)
+    val C500 = Color(0xFF171717)
+}
+
+object CommonColor {
+    val White = Color(0xFFFFFFFF)
+}
+
+object DimedColor {
+    val D070 = Color(0x0D000000).copy(alpha = 0.7f)
+    val D020 = Color(0x0D000000).copy(alpha = 0.2f)
+}
+
+object SystemColor {
+    val Success = Color(0xFF1ED45A)
+    val Warning = Color(0xFFFF6363)
+}
