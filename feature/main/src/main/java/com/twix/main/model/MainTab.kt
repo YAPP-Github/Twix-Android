f77996b6fd1c8414c1d1036f42b7020a9c58daf7
dogmania <hj296914@gmail.com>
2026-01-18 17:39:36 +0900

    ✨ Feat: GNB 탭 타이틀 추가

--- feature/main/src/main/java/com/twix/main/model/MainTab.kt
+++ feature/main/src/main/java/com/twix/main/model/MainTab.kt
@@ -6,17 +6,21 @@ import com.twix.designsystem.R
 enum class MainTab(
     val selectedIcon: Int,
     val unselectedIcon: Int,
+    val title: Int
 ) {
     HOME(
         selectedIcon = R.drawable.ic_home_selected,
-        unselectedIcon = R.drawable.ic_home_unselected
+        unselectedIcon = R.drawable.ic_home_unselected,
+        title = R.string.word_home
     ),
     STATS(
         selectedIcon = R.drawable.ic_stats_selected,
-        unselectedIcon = R.drawable.ic_stats_unselected
+        unselectedIcon = R.drawable.ic_stats_unselected,
+        title = R.string.word_stats
     ),
     COUPLE(
         selectedIcon = R.drawable.ic_couple_selected,
-        unselectedIcon = R.drawable.ic_couple_unselected
+        unselectedIcon = R.drawable.ic_couple_unselected,
+        title = R.string.word_couple_page
     )
 }
