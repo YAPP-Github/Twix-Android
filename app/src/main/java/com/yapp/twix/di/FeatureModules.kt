4282ca3766f27c648274953eb728e8f8f0aa70be
dogmania <hj296914@gmail.com>
2026-01-21 02:00:54 +0900

    ✨ Feat: HomeModule 구현

--- app/src/main/java/com/yapp/twix/di/FeatureModules.kt
+++ app/src/main/java/com/yapp/twix/di/FeatureModules.kt
@@ -1,5 +1,6 @@
 package com.yapp.twix.di
 
+import com.twix.home.di.homeModule
 import com.twix.login.di.loginModule
 import com.twix.main.di.mainModule
 import org.koin.core.module.Module
@@ -8,4 +9,5 @@ val featureModules: List<Module> =
     listOf(
         loginModule,
         mainModule,
+        homeModule
     )
