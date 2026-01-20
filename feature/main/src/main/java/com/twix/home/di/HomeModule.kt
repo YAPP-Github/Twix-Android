6527cea0328e73649533b6be757adc306b64a247
dogmania <hj296914@gmail.com>
2026-01-21 02:01:58 +0900

    ♻️ Refactor: ktlint 적용

--- feature/main/src/main/java/com/twix/home/di/HomeModule.kt
+++ feature/main/src/main/java/com/twix/home/di/HomeModule.kt
@@ -4,6 +4,7 @@ import com.twix.home.HomeViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
-val homeModule = module {
-    viewModelOf(::HomeViewModel)
-}
+val homeModule =
+    module {
+        viewModelOf(::HomeViewModel)
+    }
