48125e09d6071633b39dd0e9e9780424281f26d7
dogmania <hj296914@gmail.com>
2026-01-20 23:41:14 +0900

    ✨ Feat: mainModule DI 컨테이너 등록

--- app/src/main/java/com/yapp/twix/di/FeatureModules.kt
+++ app/src/main/java/com/yapp/twix/di/FeatureModules.kt
@@ -1,9 +1,11 @@
 package com.yapp.twix.di
 
 import com.twix.login.di.loginModule
+import com.twix.main.di.mainModule
 import org.koin.core.module.Module
 
 val featureModules: List<Module> =
     listOf(
         loginModule,
+        mainModule,
     )
