5997f8efeec5f31205fdf6aeb5f4f4ea158b14f3
dogmania <hj296914@gmail.com>
2026-01-21 02:00:39 +0900

    ✨ Feat: HomeModule 뷰모델 추가

---
+++ feature/main/src/main/java/com/twix/home/di/HomeModule.kt
@@ -0,0 +1,9 @@
+package com.twix.home.di
+
+import com.twix.home.HomeViewModel
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+val homeModule = module {
+    viewModelOf(::HomeViewModel)
+}
