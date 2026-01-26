4146605ccc844f5afe1538451d03c75b526867d8
chanho0908 <uni070@naver.com>
2026-01-26 22:13:26 +0900

    ♻️ Refactor: 미완성 코드 제거

--- feature/task-certification/src/main/java/com/twix/task_certification/TaskCertificationScreen.kt
+++ feature/task-certification/src/main/java/com/twix/task_certification/TaskCertificationScreen.kt
@@ -6,12 +6,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,8 +17,6 @@ import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.twix.designsystem.components.text.AppText
 import com.twix.designsystem.theme.GrayColor
-import com.twix.designsystem.theme.LocalStatusBarColor
-import com.twix.designsystem.theme.StatusBarStyle
 import com.twix.designsystem.theme.TwixTheme
 import com.twix.domain.model.enums.AppTextStyle
 import com.twix.task_certification.R
@@ -43,26 +39,18 @@ fun TaskCertificationRoute(
         viewModel.dispatch(TaskCertificationIntent.BindCamera(lifecycleOwner))
     }
 
-    CompositionLocalProvider(
-        LocalStatusBarColor provides
-            StatusBarStyle(
-                GrayColor.C500.toArgb(),
-                false,
-            ),
-    ) {
-        TaskCertificationScreen(
-            uiState = uiState,
-            onClickClose = {
-                navigateToBack()
-            },
-            onCaptureClick = {
-                viewModel.dispatch(TaskCertificationIntent.TakePicture)
-            },
-            onToggleCameraClick = {
-                viewModel.dispatch(TaskCertificationIntent.ToggleCamera(lifecycleOwner))
-            },
-        )
-    }
+    TaskCertificationScreen(
+        uiState = uiState,
+        onClickClose = {
+            navigateToBack()
+        },
+        onCaptureClick = {
+            viewModel.dispatch(TaskCertificationIntent.TakePicture)
+        },
+        onToggleCameraClick = {
+            viewModel.dispatch(TaskCertificationIntent.ToggleCamera(lifecycleOwner))
+        },
+    )
 }
 
 @Composable
