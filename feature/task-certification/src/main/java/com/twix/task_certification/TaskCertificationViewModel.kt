326c781753a5d199d29267ca3d8f5aa0f0867444
chanho0908 <uni070@naver.com>
2026-01-30 10:46:40 +0900

    ♻️ Refactor: 카메라 바인딩 책임을 UI로 이동

--- feature/task-certification/src/main/java/com/twix/task_certification/TaskCertificationViewModel.kt
+++ feature/task-certification/src/main/java/com/twix/task_certification/TaskCertificationViewModel.kt
@@ -3,36 +3,18 @@ package com.twix.task_certification
 import android.net.Uri
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.viewModelScope
-import com.twix.task_certification.camera.Camera
-import com.twix.task_certification.model.CameraPreview
 import com.twix.task_certification.model.TaskCertificationIntent
 import com.twix.task_certification.model.TaskCertificationSideEffect
 import com.twix.task_certification.model.TaskCertificationUiState
 import com.twix.ui.base.BaseViewModel
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
-class TaskCertificationViewModel(
-    private val camera: Camera,
-) : BaseViewModel<TaskCertificationUiState, TaskCertificationIntent, TaskCertificationSideEffect>(
+class TaskCertificationViewModel :
+    BaseViewModel<TaskCertificationUiState, TaskCertificationIntent, TaskCertificationSideEffect>(
         TaskCertificationUiState(),
     ) {
-    init {
-        camera.surfaceRequests
-            .map { request -> request?.let { CameraPreview(request) } }
-            .onEach { preview ->
-                reduce { copy(preview = preview) }
-            }.launchIn(viewModelScope)
-    }
-
     override suspend fun handleIntent(intent: TaskCertificationIntent) {
         when (intent) {
-            is TaskCertificationIntent.BindCamera -> {
-                bindCamera(intent.lifecycleOwner)
-            }
-
             is TaskCertificationIntent.TakePicture -> {
                 takePicture()
             }
@@ -48,32 +30,20 @@ class TaskCertificationViewModel(
     }
 
     private fun takePicture() {
-        camera.takePicture(
-            onComplete = {
-                updateCapturedCImage(it)
-                unbindCamera()
-            },
-            onFailure = {
-                onFailureCapture()
-            },
-        )
+//        camera.takePicture(
+//            onComplete = {
+//                updateCapturedCImage(it)
+//                unbindCamera()
+//            },
+//            onFailure = {
+//                onFailureCapture()
+//            },
+//        )
     }
 
     private fun onFailureCapture() {
         viewModelScope.launch {
             emitSideEffect(TaskCertificationSideEffect.ImageCaptureFailException)
-        }
-    }
-
-    private fun unbindCamera() {
-        viewModelScope.launch {
-            camera.unbind()
-        }
-    }
-
-    private fun bindCamera(lifecycleOwner: LifecycleOwner) {
-        viewModelScope.launch {
-            camera.bind(lifecycleOwner, uiState.value.lens)
         }
     }
 
@@ -90,11 +60,11 @@ class TaskCertificationViewModel(
 
     private fun toggleCamera(lifecycleOwner: LifecycleOwner) {
         reduce { toggleLens() }
-        bindCamera(lifecycleOwner)
+        // bindCamera(lifecycleOwner)
     }
 
     private fun toggleTorch() {
         reduce { toggleTorch() }
-        camera.toggleTorch(uiState.value.torch)
+        // camera.toggleTorch(uiState.value.torch)
     }
 }
--- feature/task-certification/src/main/java/com/twix/task_certification/camera/Camera.kt
+++ feature/task-certification/src/main/java/com/twix/task_certification/camera/Camera.kt
@@ -3,13 +3,13 @@ package com.twix.task_certification.camera
 import android.net.Uri
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.SurfaceRequest
 import androidx.lifecycle.LifecycleOwner
+import com.twix.task_certification.model.CameraPreview
 import com.twix.task_certification.model.TorchStatus
 import kotlinx.coroutines.flow.StateFlow
 
 interface Camera {
-    val surfaceRequests: StateFlow<SurfaceRequest?>
+    val surfaceRequests: StateFlow<CameraPreview?>
 
     suspend fun bind(
         lifecycleOwner: LifecycleOwner,
