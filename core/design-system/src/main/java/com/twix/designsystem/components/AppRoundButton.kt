3993ff403baccb5a56e55957a85a4177e7baa74f
chanho0908 <uni070@naver.com>
2026-01-30 23:48:48 +0900

    ✨ Feat: 공용 Round Button 컴포저블 추가

---
+++ core/design-system/src/main/java/com/twix/designsystem/components/AppRoundButton.kt
@@ -0,0 +1,99 @@
+package com.twix.designsystem.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.twix.designsystem.components.text.AppText
+import com.twix.designsystem.theme.CommonColor
+import com.twix.designsystem.theme.GrayColor
+import com.twix.designsystem.theme.TwixTheme
+import com.twix.domain.model.enums.AppTextStyle
+
+@Composable
+fun AppRoundButton(
+    modifier: Modifier = Modifier,
+    borderColor: Color = GrayColor.C500,
+    backgroundColor: Color = CommonColor.White,
+    content: @Composable () -> Unit,
+) {
+    Box(
+        modifier = modifier,
+    ) {
+        Box(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .offset(y = 4.dp)
+                    .background(
+                        color = borderColor,
+                        shape = RoundedCornerShape(100),
+                    ),
+        )
+
+        Box(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .background(
+                        color = backgroundColor,
+                        shape = RoundedCornerShape(100),
+                    ).border(
+                        width = 1.6.dp,
+                        color = borderColor,
+                        shape = RoundedCornerShape(100),
+                    ),
+            contentAlignment = Alignment.Center,
+        ) {
+            content()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppRoundButtonPreview() {
+    TwixTheme {
+        Column {
+            AppRoundButton(
+                modifier =
+                    Modifier
+                        .width(330.dp)
+                        .height(68.dp),
+            ) {
+                AppText(
+                    style = AppTextStyle.T2,
+                    color = GrayColor.C500,
+                    text = "버튼임니다",
+                )
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            AppRoundButton(
+                backgroundColor = GrayColor.C500,
+                modifier =
+                    Modifier
+                        .width(330.dp)
+                        .height(68.dp),
+            ) {
+                AppText(
+                    style = AppTextStyle.T2,
+                    color = CommonColor.White,
+                    text = "버튼임니다",
+                )
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+    }
+}
