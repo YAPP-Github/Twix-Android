303927bff1cbf96e139be310a2d9ae0dbb776ac0
chanho0908 <uni070@naver.com>
2026-01-31 13:06:20 +0900

    ✨ Feat: 코멘트 작성을 위한 원형 텍스트 필드 구현

---
+++ core/design-system/src/main/java/com/twix/designsystem/components/comment/CommentTextField.kt
@@ -0,0 +1,135 @@
+package com.twix.designsystem.components.comment
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.twix.designsystem.R
+import com.twix.designsystem.components.comment.model.CommentUiModel
+import com.twix.designsystem.theme.TwixTheme
+
+private val CIRCLE_PADDING_START: Dp = 50.dp
+private val CIRCLE_SIZE: Dp = 64.dp
+private val CIRCLE_GAP: Dp = CIRCLE_PADDING_START - CIRCLE_SIZE
+
+@Composable
+fun CommentTextField(
+    uiModel: CommentUiModel,
+    onCommentChanged: (TextFieldValue) -> Unit,
+    onFocusChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val focusRequester = remember { FocusRequester() }
+    val interactionSource = remember { MutableInteractionSource() }
+    val placeholder = stringResource(R.string.comment_text_field_placeholder)
+
+    Box(
+        modifier =
+            modifier
+                .clickable(
+                    indication = null,
+                    interactionSource = interactionSource,
+                ) {
+                    focusRequester.requestFocus()
+                },
+    ) {
+        BasicTextField(
+            value = uiModel.comment,
+            onValueChange = { newValue -> onCommentChanged(newValue) },
+            modifier =
+                Modifier
+                    .alpha(0f)
+                    .focusRequester(focusRequester)
+                    .onFocusChanged { focusState ->
+                        onFocusChanged(focusState.isFocused)
+                    },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            singleLine = true,
+        )
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(CIRCLE_GAP),
+            modifier =
+                Modifier.drawWithCache {
+                    val radius = size.height / 2
+                    val paddingStart = CIRCLE_PADDING_START.toPx()
+
+                    onDrawBehind {
+                        repeat(CommentUiModel.COMMENT_COUNT) { index ->
+                            val cx = radius + index * paddingStart
+
+                            drawCircle(
+                                color = Color.Black,
+                                radius = radius,
+                                center = Offset(cx, radius),
+                                style = Stroke(2.dp.toPx()),
+                            )
+                        }
+                    }
+                },
+        ) {
+            repeat(CommentUiModel.COMMENT_COUNT) { index ->
+                val char =
+                    if (uiModel.hidePlaceholder) {
+                        uiModel.comment.text
+                            .getOrNull(index)
+                            ?.toString()
+                    } else {
+                        placeholder.getOrNull(index)?.toString()
+                    }.orEmpty()
+
+                CommentCircle(
+                    text = char,
+                    showPlaceholder = !uiModel.hidePlaceholder,
+                    showCursor = uiModel.showCursor(index),
+                    modifier =
+                        Modifier.clickable(
+                            indication = null,
+                            interactionSource = interactionSource,
+                        ) {
+                            focusRequester.requestFocus()
+                            onCommentChanged(uiModel.comment.copy(selection = TextRange(uiModel.comment.text.length)))
+                        },
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CommentTextFieldPreview() {
+    TwixTheme {
+        var text by remember { mutableStateOf(TextFieldValue("")) }
+        var isFocused by remember { mutableStateOf(false) }
+        CommentTextField(
+            uiModel = CommentUiModel(text, isFocused),
+            onCommentChanged = { text = it },
+            onFocusChanged = { isFocused = it },
+        )
+    }
+}
---
+++ core/design-system/src/main/java/com/twix/designsystem/components/comment/model/CommentUiModel.kt
@@ -0,0 +1,44 @@
+package com.twix.designsystem.components.comment.model
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.text.input.TextFieldValue
+
+@Immutable
+data class CommentUiModel(
+    val comment: TextFieldValue = TextFieldValue(""),
+    val isFocused: Boolean = false,
+) {
+    fun updateComment(comment: TextFieldValue) = copy(comment = comment.copy(comment.text.take(COMMENT_COUNT)))
+
+    fun updateFocus(isFocused: Boolean) = copy(isFocused = isFocused)
+
+    /**
+     * 특정 index 위치에 커서를 표시할지 여부를 반환한다.
+     *
+     * 표시 조건
+     * 1. 포커스 상태일 것
+     * 2. 현재 selection 시작 위치가 해당 index 일 것
+     * 3. 해당 위치에 문자가 없을 것 (빈 칸)
+     *
+     * @param index 확인할 문자 위치
+     */
+    fun showCursor(index: Int): Boolean {
+        val isCharEmpty = comment.text.getOrNull(index) == null
+        return isFocused && comment.selection.start == index && isCharEmpty
+    }
+
+    /**
+     * 플레이스홀더 표시 여부.
+     *
+     * 표시 조건
+     * - 포커스 중이 아니거나
+     * - 텍스트가 하나라도 존재하지 않으면
+     *
+     */
+    val hidePlaceholder: Boolean
+        get() = isFocused || comment.text.isNotEmpty()
+
+    companion object {
+        const val COMMENT_COUNT = 5
+    }
+}
