85719ae403311c1ee489766f42cc38e966efb732
chanho0908 <uni070@naver.com>
2026-02-01 18:01:15 +0900

    ✨ Feat: 온보딩 화면 Topbar 추가

---
+++ feature/onboarding/src/main/java/com/peto/onboarding/profile/component/OnBoardingTopbar.kt
@@ -0,0 +1,39 @@
+package com.peto.onboarding.profile.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.twix.ui.extension.noRippleClickable
+import com.twix.designsystem.R as DesR
+
+@Composable
+fun OnBoardingTopbar(
+    modifier: Modifier = Modifier,
+    onBack: () -> Unit,
+) {
+    Box(
+        modifier =
+            modifier
+                .fillMaxWidth()
+                .height(72.dp),
+    ) {
+        Icon(
+            painter = painterResource(id = DesR.drawable.ic_arrow1_m_left),
+            contentDescription = "back",
+            modifier =
+                Modifier
+                    .size(44.dp)
+                    .padding(start = 10.dp)
+                    .noRippleClickable { onBack() }
+                    .align(Alignment.CenterStart),
+        )
+    }
+}
