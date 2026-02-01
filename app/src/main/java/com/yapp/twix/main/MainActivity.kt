33bba6c643df18eae5a1f29f12f6c9f37dd001ef
chanho0908 <uni070@naver.com>
2026-02-01 13:24:43 +0900

    ✨ Feat: enableEdgeToEdge 적용

--- app/src/main/java/com/yapp/twix/main/MainActivity.kt
+++ app/src/main/java/com/yapp/twix/main/MainActivity.kt
@@ -3,11 +3,13 @@ package com.yapp.twix.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.twix.designsystem.theme.TwixTheme
 import com.twix.navigation.AppNavHost
 import com.twix.ui.toast.ToastHost
@@ -17,15 +19,16 @@ import org.koin.android.ext.android.inject
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
             val toastManager by inject<ToastManager>()
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+            WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = true
 
             TwixTheme {
                 Box(
                     modifier =
                         Modifier
-                            .safeContentPadding()
                             .fillMaxSize(),
                 ) {
                     AppNavHost()
