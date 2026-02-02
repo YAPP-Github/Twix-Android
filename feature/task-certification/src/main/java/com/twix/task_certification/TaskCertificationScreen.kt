6d72767d56b480b4bda70dd7cdeaa54bcc51f720
chanho0908 <uni070@naver.com>
2026-02-03 00:58:38 +0900

    ♻️ Refactor: lifecycle의 coroutineScope에서 rememberCoroutineScope로 수정

--- feature/task-certification/src/main/java/com/twix/task_certification/TaskCertificationScreen.kt
+++ feature/task-certification/src/main/java/com/twix/task_certification/TaskCertificationScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -49,7 +50,7 @@ fun TaskCertificationRoute(
     val cameraPreview by camera.surfaceRequests.collectAsStateWithLifecycle()
 
     val lifecycleOwner = LocalLifecycleOwner.current
-    val coroutineScope = lifecycleOwner.lifecycle.coroutineScope
+    val coroutineScope = rememberCoroutineScope()
     val context = androidx.compose.ui.platform.LocalContext.current
 
     var hasPermission by remember {
