1f2d5be1563b39f612febf4435653958e5c4ed7d
dogmania <hj296914@gmail.com>
2026-02-06 02:50:20 +0900

    ✨ Feat: fromApi 메서드 구현

--- domain/src/main/java/com/twix/domain/model/enums/RepeatCycle.kt
+++ domain/src/main/java/com/twix/domain/model/enums/RepeatCycle.kt
@@ -1,7 +1,12 @@
 package com.twix.domain.model.enums
 
-enum class RepeatType {
+enum class RepeatCycle {
     DAILY,
     WEEKLY,
     MONTHLY,
+    ;
+
+    companion object {
+        fun fromApi(value: String): RepeatCycle = runCatching { valueOf(value) }.getOrElse { DAILY }
+    }
 }
