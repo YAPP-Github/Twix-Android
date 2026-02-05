865e20fdf755e147112da1709636b2dea15a58ed
chanho0908 <uni070@naver.com>
2026-02-06 02:51:19 +0900

    ✨ Feat: AuthRepository 의존성 주입 추가

--- data/src/main/java/com/twix/data/di/RepositoryModule.kt
+++ data/src/main/java/com/twix/data/di/RepositoryModule.kt
@@ -1,6 +1,8 @@
 package com.twix.data.di
 
+import com.twix.data.repository.DefaultAuthRepository
 import com.twix.data.repository.DefaultOnboardingRepository
+import com.twix.domain.repository.AuthRepository
 import com.twix.domain.repository.OnBoardingRepository
 import org.koin.dsl.module
 
@@ -8,5 +10,8 @@ internal val repositoryModule =
     module {
         single<OnBoardingRepository> {
             DefaultOnboardingRepository(get())
+        }
+        single<AuthRepository> {
+            DefaultAuthRepository(get(), get())
         }
     }
