84a796a7801974d3c89501552ed0106468783348
dogmania <hj296914@gmail.com>
2026-02-06 02:50:46 +0900

    ✨ Feat: 목표 조회 비즈니스 로직 연결 및 상태 처리

--- feature/main/src/main/java/com/twix/home/HomeSideEffect.kt
+++ feature/main/src/main/java/com/twix/home/HomeSideEffect.kt
@@ -1,7 +1,14 @@
 package com.twix.home
 
+import androidx.annotation.StringRes
+import com.twix.designsystem.components.toast.model.ToastType
 import com.twix.ui.base.SideEffect
 
 interface HomeSideEffect : SideEffect {
     data object ShowMonthPickerBottomSheet : HomeSideEffect
+
+    data class ShowToast(
+        @param:StringRes val resId: Int,
+        val type: ToastType,
+    ) : HomeSideEffect
 }
