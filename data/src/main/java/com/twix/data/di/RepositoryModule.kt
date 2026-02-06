e5f4de67833ed5f1835f20ae928aeee4e63c5edf
dogmania <hj296914@gmail.com>
2026-02-06 15:55:47 +0900

    ♻️ Refactor: import 알바펫 순으로 정렬

--- data/src/main/java/com/twix/data/di/RepositoryModule.kt
+++ data/src/main/java/com/twix/data/di/RepositoryModule.kt
@@ -1,10 +1,10 @@
 package com.twix.data.di
 
-import com.twix.data.repository.DefaultGoalRepository
 import com.twix.data.repository.DefaultAuthRepository
+import com.twix.data.repository.DefaultGoalRepository
 import com.twix.data.repository.DefaultOnboardingRepository
-import com.twix.domain.repository.GoalRepository
 import com.twix.domain.repository.AuthRepository
+import com.twix.domain.repository.GoalRepository
 import com.twix.domain.repository.OnBoardingRepository
 import org.koin.dsl.module
 
