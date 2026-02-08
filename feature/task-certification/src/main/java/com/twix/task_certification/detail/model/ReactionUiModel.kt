d1e7e9d544494206ba3ff50613e45df7b3c7c1b9
chanho0908 <uni070@naver.com>
2026-02-09 03:15:12 +0900

    ✨ Feat: ReactionBox 컴포넌트 구현

--- feature/task-certification/src/main/java/com/twix/task_certification/detail/model/ReactionUiModel.kt
+++ feature/task-certification/src/main/java/com/twix/task_certification/detail/model/ReactionUiModel.kt
@@ -1,15 +1,15 @@
 package com.twix.task_certification.detail.model
 
+import com.twix.designsystem.R
 import com.twix.domain.model.enums.GoalReactionType
-import com.twix.designsystem.R as DesR
 
 enum class ReactionUiModel(
     val type: GoalReactionType,
     val imageResources: Int,
 ) {
-    HAPPY(GoalReactionType.HAPPY, DesR.drawable.ic_keepi_happy),
-    TROUBLE(GoalReactionType.TROUBLE, DesR.drawable.ic_keepi_trouble),
-    LOVE(GoalReactionType.LOVE, DesR.drawable.ic_keepi_love),
-    DOUBT(GoalReactionType.DOUBT, DesR.drawable.ic_keepi_doubt),
-    FUCK(GoalReactionType.FUCK, DesR.drawable.ic_keepi_fuck),
+    HAPPY(GoalReactionType.HAPPY, R.drawable.ic_keepi_happy),
+    TROUBLE(GoalReactionType.TROUBLE, R.drawable.ic_keepi_trouble),
+    LOVE(GoalReactionType.LOVE, R.drawable.ic_keepi_love),
+    DOUBT(GoalReactionType.DOUBT, R.drawable.ic_keepi_doubt),
+    FUCK(GoalReactionType.FUCK, R.drawable.ic_keepi_fuck),
 }
