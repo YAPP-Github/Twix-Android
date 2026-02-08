69e31826f1baf5fb6e7e22c5387cb18f20f06146
chanho0908 <uni070@naver.com>
2026-02-08 21:41:34 +0900

    ✨ Feat: CertificationCard 컴포넌트 구현

---
+++ feature/task-certification/src/main/java/com/twix/task_certification/detail/component/CertificationCard.kt
@@ -0,0 +1,56 @@
+package com.twix.task_certification.detail.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.twix.designsystem.theme.TwixTheme
+
+@Composable
+internal fun CertificationCard(
+    background: Color,
+    borderColor: Color,
+    rotation: Float = 0f,
+    content: @Composable BoxScope.() -> Unit = {},
+) {
+    Box(
+        modifier =
+            Modifier
+                .padding(horizontal = 27.dp)
+                .fillMaxWidth()
+                .aspectRatio(1f)
+                .rotate(rotation)
+                .clip(shape = RoundedCornerShape(12.dp))
+                .border(
+                    width = 1.6.dp,
+                    color = borderColor,
+                    shape = RoundedCornerShape(12.dp),
+                ).background(background),
+        contentAlignment = Alignment.Center,
+        content = content,
+    )
+}
+
+@Preview
+@Composable
+fun CertificationCardPreview() {
+    TwixTheme {
+        CertificationCard(
+            rotation = 0f,
+            background = Color.White,
+            borderColor = Color.Black,
+        )
+    }
+}
