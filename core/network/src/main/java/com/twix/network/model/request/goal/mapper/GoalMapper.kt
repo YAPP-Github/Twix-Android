441554ef3e8393265a6a82821a216ea9b48628f8
dogmania <hj296914@gmail.com>
2026-02-09 18:41:01 +0900

    ✨ Feat: UpdateGoalParam -> UpdateGoalRequest Mapper 구현

--- core/network/src/main/java/com/twix/network/model/request/goal/mapper/GoalMapper.kt
+++ core/network/src/main/java/com/twix/network/model/request/goal/mapper/GoalMapper.kt
@@ -1,7 +1,9 @@
 package com.twix.network.model.request.goal.mapper
 
 import com.twix.domain.model.goal.CreateGoalParam
+import com.twix.domain.model.goal.UpdateGoalParam
 import com.twix.network.model.request.goal.model.CreateGoalRequest
+import com.twix.network.model.request.goal.model.UpdateGoalRequest
 
 fun CreateGoalParam.toRequest(): CreateGoalRequest =
     CreateGoalRequest(
@@ -10,5 +12,14 @@ fun CreateGoalParam.toRequest(): CreateGoalRequest =
         repeatCycle = repeatCycle.toApi(),
         repeatCount = repeatCount,
         startDate = startDate.toString(),
+        endDate = endDate?.toString(),
+    )
+
+fun UpdateGoalParam.toRequest(): UpdateGoalRequest =
+    UpdateGoalRequest(
+        name = name,
+        icon = icon.toApi(),
+        repeatCycle = repeatCycle.toApi(),
+        repeatCount = repeatCount,
         endDate = endDate?.toString(),
     )
