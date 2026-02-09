23ec8bda1fdd04f223d07fc5f2b272ef2ff668b6
dogmania <hj296914@gmail.com>
2026-02-10 02:26:34 +0900

    ✨ Feat: privacyPolicy URL 환경변수 설정

--- feature/settings/build.gradle.kts
+++ feature/settings/build.gradle.kts
@@ -1,7 +1,39 @@
+import java.util.Properties
+import kotlin.apply
+
 plugins {
     alias(libs.plugins.twix.feature)
 }
 
+val localProperties =
+    Properties().apply {
+        val localPropertiesFile = rootProject.file("local.properties")
+        if (localPropertiesFile.exists()) {
+            load(localPropertiesFile.inputStream())
+        }
+    }
+
 android {
     namespace = "com.twix.settings"
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    val privacyPolicyUrl =
+        localProperties.getProperty("privacy_policy_url")
+            ?: error("privacy_policy_url is missing in local.properties")
+
+    buildTypes {
+        debug {
+            buildConfigField("String", "PRIVACY_POLICY_URL", "\"$privacyPolicyUrl\"")
+        }
+        release {
+            buildConfigField("String", "PRIVACY_POLICY_URL", "\"$privacyPolicyUrl\"")
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.browser)
 }
