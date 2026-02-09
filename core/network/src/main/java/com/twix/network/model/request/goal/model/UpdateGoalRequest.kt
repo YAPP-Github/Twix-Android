faad6c28c05fe4e77f477b2d4c24788ddbe82bea
dogmania <hj296914@gmail.com>
2026-02-09 18:41:15 +0900

    ✨ Feat: 목표 수정 요청 DTO 구현

---
+++ core/network/src/main/java/com/twix/network/model/request/goal/model/UpdateGoalRequest.kt
@@ -0,0 +1,13 @@
+package com.twix.network.model.request.goal.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UpdateGoalRequest(
+    @SerialName("name") val name: String,
+    @SerialName("icon") val icon: String,
+    @SerialName("repeatCycle") val repeatCycle: String,
+    @SerialName("repeatCount") val repeatCount: Int,
+    @SerialName("endDate") val endDate: String? = null,
+)
