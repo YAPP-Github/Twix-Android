4d5543b03db3ac4dee21f7d141f67fee918e28f0
dogmania <hj296914@gmail.com>
2026-02-09 18:40:32 +0900

    ♻️ Refactor: 네이밍 변경

--- core/network/src/main/java/com/twix/network/model/response/goal/model/GoalDetailResponse.kt
+++ core/network/src/main/java/com/twix/network/model/response/goal/model/GoalDetailResponse.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class CreateGoalResponse(
+data class GoalDetailResponse(
     @SerialName("goalId") val goalId: Long,
     @SerialName("name") val name: String,
     @SerialName("icon") val icon: String,
