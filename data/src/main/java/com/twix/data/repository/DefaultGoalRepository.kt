baa20ae7f71753abde503f71c5ca9a1ce270ebec
dogmania <hj296914@gmail.com>
2026-02-09 17:00:04 +0900

    ✨ Feat: 목표 생성 API 통신 로직 구현

--- data/src/main/java/com/twix/data/repository/DefaultGoalRepository.kt
+++ data/src/main/java/com/twix/data/repository/DefaultGoalRepository.kt
@@ -1,8 +1,11 @@
 package com.twix.data.repository
 
+import com.twix.domain.model.goal.CreateGoalParam
+import com.twix.domain.model.goal.CreatedGoal
 import com.twix.domain.model.goal.GoalList
 import com.twix.domain.repository.GoalRepository
 import com.twix.network.execute.safeApiCall
+import com.twix.network.model.request.goal.mapper.toRequest
 import com.twix.network.model.response.goal.mapper.toDomain
 import com.twix.network.service.GoalService
 import com.twix.result.AppResult
@@ -11,4 +14,9 @@ class DefaultGoalRepository(
     private val service: GoalService,
 ) : GoalRepository {
     override suspend fun fetchGoalList(date: String): AppResult<GoalList> = safeApiCall { service.fetchGoals(date).toDomain() }
+
+    override suspend fun createGoal(param: CreateGoalParam): AppResult<CreatedGoal> =
+        safeApiCall {
+            service.createGoal(param.toRequest()).toDomain()
+        }
 }
