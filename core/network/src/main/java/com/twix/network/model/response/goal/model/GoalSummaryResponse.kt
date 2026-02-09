ee46d30d9a5845c9e4d7980263574519462631a6
dogmania <hj296914@gmail.com>
2026-02-10 02:57:04 +0900

    ✨ Feat: GoalSummaryResponse 구현

---
+++ core/network/src/main/java/com/twix/network/model/response/goal/model/GoalSummaryResponse.kt
@@ -0,0 +1,14 @@
+package com.twix.network.model.response.goal.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GoalSummaryResponse(
+    @SerialName("goalId") val goalId: Long,
+    @SerialName("name") val name: String,
+    @SerialName("icon") val icon: String,
+    @SerialName("repeatCycle") val repeatCycle: String,
+    @SerialName("startDate") val startDate: String,
+    @SerialName("endDate") val endDate: String?,
+)
