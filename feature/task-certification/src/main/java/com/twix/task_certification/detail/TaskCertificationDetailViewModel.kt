d9823eb939a657cf8d4627d4f36771f80d9a6b3f
chanho0908 <uni070@naver.com>
2026-02-12 10:07:25 +0900

    ♻️ Refactor: 오타 수정

--- feature/task-certification/src/main/java/com/twix/task_certification/detail/TaskCertificationDetailViewModel.kt
+++ feature/task-certification/src/main/java/com/twix/task_certification/detail/TaskCertificationDetailViewModel.kt
@@ -84,6 +84,6 @@ class TaskCertificationDetailViewModel(
 
     companion object {
         private const val GOAL_ID_NOT_FOUND = "Goal Id Argument Not Found"
-        private const val TARGET_DATE_NOT_FOUND = "Target Date Id Argument Not Found"
+        private const val TARGET_DATE_NOT_FOUND = "Target Date Argument Not Found"
     }
 }
