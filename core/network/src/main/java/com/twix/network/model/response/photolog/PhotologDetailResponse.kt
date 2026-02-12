dd649181509906c6b26fe443577646f40668432b
chanho0908 <uni070@naver.com>
2026-02-12 10:08:27 +0900

    ♻️ Refactor: PhotologDetailResponse default parameter 제거

--- core/network/src/main/java/com/twix/network/model/response/photolog/PhotologDetailResponse.kt
+++ core/network/src/main/java/com/twix/network/model/response/photolog/PhotologDetailResponse.kt
@@ -8,7 +8,7 @@ data class PhotologDetailResponse(
     @SerialName("photologId") val photologId: Long,
     @SerialName("goalId") val goalId: Long,
     @SerialName("imageUrl") val imageUrl: String,
-    @SerialName("comment") val comment: String? = null,
+    @SerialName("comment") val comment: String?,
     @SerialName("verificationDate") val verificationDate: String,
     @SerialName("uploaderName") val uploaderName: String,
     @SerialName("uploadedAt") val uploadedAt: String,
